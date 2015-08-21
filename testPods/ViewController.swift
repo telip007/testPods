26e38eb5bb51a6979ddb57594bfa7c4db13f9e6d
telip007 <telip007@icloud.com>
2015-08-22 00:24:32 +0200

    Initial Commit

---
+++ testPods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testPods
+//
+//  Created by Talip Göksu on 22.08.15.
+//  Copyright © 2015 telip007. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
